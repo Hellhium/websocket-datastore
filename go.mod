d8b57d82a33a0c838b99733f4134cfc2be65dab5
Jemy SCHNEPP <jemy.schnepp@toastate.com>
2021-11-14 15:14:46 +0100

    + apidocs

--- go.mod
+++ go.mod
@@ -1,12 +1,20 @@
 module github.com/hellhium/wstest
 
-go 1.15
+go 1.17
 
 require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
 	github.com/go-kit/kit v0.12.0
 	github.com/gorilla/websocket v1.4.2
 	github.com/julienschmidt/httprouter v1.3.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- routes/routes.go
+++ routes/routes.go
@@ -1,25 +1,48 @@
 package routes
 
 import (
-	"encoding/json"
 	"net/http"
 
+	"github.com/hellhium/wstest/lib/httphelpers"
 	"github.com/hellhium/wstest/lib/router"
 	"github.com/hellhium/wstest/lib/router/routebuilders"
 	"github.com/hellhium/wstest/lib/tlib"
 	"github.com/julienschmidt/httprouter"
 
+	_ "embed"
+
 	_ "github.com/hellhium/wstest/routes/api" // API routes
 	_ "github.com/hellhium/wstest/routes/ws"  // Websocket routes
 )
 
+//go:embed swagger.html
+var swaggerHtml []byte
+
+//go:embed swagger.yml
+var swaggerYml []byte
+
 func init() {
-	router.Router.GET("/", routebuilders.NonAuthed(r))
+	router.Router.GET("/status", routebuilders.NonAuthed(r))
+	router.Router.GET("/", routebuilders.NonAuthed(apidoc))
+	router.Router.GET("/swagger.yml", routebuilders.NonAuthed(apidocyml))
+}
+
+func apidoc(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	resp.Header().Add("content-type", "text/html")
+	resp.Write(swaggerHtml)
+}
+
+func apidocyml(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	resp.Header().Add("content-type", "text/yaml")
+	resp.Write(swaggerYml)
+
 }
 
 func r(resp http.ResponseWriter, req *http.Request, params httprouter.Params) {
-	json.NewEncoder(resp).Encode(map[string]interface{}{
-		"success": true,
+	res := httphelpers.NewResp()
+	res.Data = map[string]interface{}{
 		"version": tlib.Ver(),
-	})
+	}
+	res.Success = true
+	res.R(resp)
 }
